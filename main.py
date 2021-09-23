8f021bb2c5718b0831026f878447fe2517fc0e17
clong <clong389@gmail.com>
2021-09-23 17:19:19 -0400

    added code for keystroke detection

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ import pygame
 #Initialize pygame
 pygame.init()
 
-#Create game window Establish screen width, height
+#Create game window Set screen width, height
 screen = pygame.display.set_mode((800,600))
 #Run_window flashes but does not persist until master loop is created
 
 #Game window_change title and icon
 pygame.display.set_caption('Galactic Target Practice')
-icon=pygame.image.load('alien.png')
+icon = pygame.image.load('alien.png')
 pygame.display.set_icon(icon)
 
 #player ship graphic and coordinates
 playerImg = pygame.Image.load('space-invaders.png')
-playerX = 370
-playerY = 480
-#draw player graphic on screen using method .blit
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+playerX = 370 #x axis
+playerY = 480 #y axis
+
+#create player function
+def player(x, y):
+#draw player graphic on screen using method .blit    
+    screen.blit(playerImg, (x, y))
 
 #Game Master Loop
-#Allows game window to stay open or persist
+#Allows game window to stay open while condition is met
 running = True
 while running:
     #Creates exit from infinite loop
@@ -31,9 +33,19 @@ while running:
         if event.type == pygame.QUIT():
             running = False #Loop stops when someone tries to exit
 
+#If keystroke pressed, note direction
+if event.type == pygame.KEYDOWN:
+    if event.key == pygame.K_LEFT:
+        print('left arrow is pressed')
+    if event.key == pygame.K_RIGHT:
+        print('right arrow is pressed')
+if event.type == pygame.KEYUP:
+    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        print('keystroke has been released')
+
 #change color of game window background
-screen.fill((0,0,0))#black
-#draw player ship on screen background
-player()
-pygame.display.update()#game display continually updates
+screen.fill((0,0,0)) #black
+player(playerX, playerY) #calling player to screen Allow for coordinates to change
+pygame.display.update() #game display continually updates
+
 
