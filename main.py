d4cb191951dbb8ff0f001d9403ff46105c0da33e
clong <clong389@gmail.com>
2021-09-23 11:59:27 -0400

    created images file and initialized pygame in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import math
+import pygame
+
+#Initialize pygame
+pygame.init()
+#Create game window Establish screen width, height
+screen = pygame.display.set_mode((800,600))
+#Run_window flashes but does not persist
+
+#Game window_change title and icon
+pygame.display.set_caption('Universal Target Practice')
