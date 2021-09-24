9a8f9f581e7090ab0f2a747633cc53c1fc966a7b
clong <clong389@gmail.com>
2021-09-23 20:01:52 -0400

    troubleshooting problem images not found

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 #import modules
-import math
 import pygame
+import math
 
 #Initialize pygame
 pygame.init()
 
-#Create game window Set screen width, height
+#Game window screen width, height
 screen = pygame.display.set_mode((800,600))
-#Run_window flashes but does not persist until master loop is created
+#window flashes but does not persist until master loop is created
 
-#Game window_change title and icon
+#Game window change title and icon
 pygame.display.set_caption('Galactic Target Practice')
 icon = pygame.image.load('alien.png')
 pygame.display.set_icon(icon)
 
-#player ship graphic and coordinates
-playerImg = pygame.Image.load('space-invaders.png')
+#load player image and coordinates
+playerImg = pygame.image.load('space-invaders.png')
 playerX = 370 #x axis
 playerY = 480 #y axis
 
-#create player function
+#draw player image on the screen
+#send in changing values of x y coordinates
 def player(x, y):
-#draw player graphic on screen using method .blit    
     screen.blit(playerImg, (x, y))
 
 #Game Master Loop
 #Allows game window to stay open while condition is met
 running = True
 while running:
+    
+    #change color of game window background
+    screen.fill((0,0,0)) #black
+    
     #Creates exit from infinite loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT():
             running = False #Loop stops when someone tries to exit
+
+    #After screen fills, player is drawn on top of filled screen
+    player(playerX, playerY) #calling player to screen Allow for coordinates to change
+    pygame.display.update() #game display continually updates
 
 #If keystroke pressed, note direction
 if event.type == pygame.KEYDOWN:
@@ -43,9 +51,6 @@ if event.type == pygame.KEYUP:
     if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
         print('keystroke has been released')
 
-#change color of game window background
-screen.fill((0,0,0)) #black
-player(playerX, playerY) #calling player to screen Allow for coordinates to change
-pygame.display.update() #game display continually updates
+
 
 
